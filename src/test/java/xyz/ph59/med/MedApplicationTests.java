59298e8e67c725c6463eb98ae0cec97b6a6780ad
ph59x <ymoonl@outlook.com>
2025-03-28 18:15:36 +0800

    Init

---
+++ src/test/java/xyz/ph59/med/MedApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ph59.med;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
