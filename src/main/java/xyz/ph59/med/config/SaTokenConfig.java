c436084193dfc4e3d5476d4c072a4b019f2b4bd8
ph59x <ymoonl@outlook.com>
2025-04-11 11:04:38 +0800

    fix: 注解鉴权未启用

--- src/main/java/xyz/ph59/med/config/SaTokenConfig.java
+++ src/main/java/xyz/ph59/med/config/SaTokenConfig.java
@@ -1,6 +1,7 @@
 package xyz.ph59.med.config;
 
 import cn.dev33.satoken.filter.SaServletFilter;
+import cn.dev33.satoken.interceptor.SaInterceptor;
 import cn.dev33.satoken.jwt.StpLogicJwtForSimple;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpLogic;
@@ -9,9 +10,11 @@ import cn.dev33.satoken.util.SaResult;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SaTokenConfig {
+public class SaTokenConfig implements WebMvcConfigurer {
     @Bean
     public StpLogic getStpLogicJwt() {
         return new StpLogicJwtForSimple();
@@ -29,7 +32,12 @@ public class SaTokenConfig {
                     // TODO 自定义返回消息
                     return new SaResult(HttpStatus.UNAUTHORIZED.value(), null, null);
                 });
+    }
 
-
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SaInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/auth/**");
     }
 }
