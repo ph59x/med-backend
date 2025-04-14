47440c714acd877bd3fbc9441d701ca248822d75
ph59x <ymoonl@outlook.com>
2025-04-14 16:13:15 +0800

    feat: 医生与用户关系缓存

--- src/main/java/xyz/ph59/med/service/RelationService.java
+++ src/main/java/xyz/ph59/med/service/RelationService.java
@@ -2,19 +2,44 @@ package xyz.ph59.med.service;
 
 import cn.dev33.satoken.annotation.SaCheckRole;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import xyz.ph59.med.mapper.RelationMapper;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RelationService {
+    private static final int DOCTOR_USER_RELATION_TTL = 1800;
+
     private final RelationMapper relationMapper;
+    private final RedisTemplate<Object, Object> redisTemplate;
 
     @SaCheckRole("DOCTOR")
     public List<Integer> getUserRelation(int drId) {
-        return relationMapper.selectRelatedUser(drId);
-        // TODO hashmap缓存
+        String key = "doctorUserRelation:" + drId;
+        if (redisTemplate.hasKey(key)) {
+            return redisTemplate.opsForSet()
+                    .members(key)
+                    .stream()
+                    .map(object -> (Integer) object)
+                    .collect(Collectors.toList());
+        }
+        else {
+            return this.refreshUserRelation(drId);
+        }
+    }
+
+    public List<Integer> refreshUserRelation(int drId) {
+        String key = "doctorUserRelation:" + drId;
+        List<Integer> relatedUser = relationMapper.selectRelatedUser(drId);
+        for (Integer i : relatedUser) {
+            redisTemplate.opsForSet().add(key, i);
+        }
+        redisTemplate.expire(key, DOCTOR_USER_RELATION_TTL, TimeUnit.SECONDS);
+        return relatedUser;
     }
 }
